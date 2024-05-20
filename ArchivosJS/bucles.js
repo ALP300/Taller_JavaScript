2b8e26599243c9abb0c67c84761b193f8ca69262
ALP300 <aitorpalomino1@gmail.com>
2024-05-19 19:54:49 -0500

    html y css

---
+++ ArchivosJS/bucles.js
@@ -0,0 +1,5 @@
+let suma=0;
+for(let i=0; i<=100; i++){
+    suma+=i;
+}
+console.log("La suma de los números del 1 al 100 es: ", suma);
---
+++ ArchivosJS/bucles2.js
@@ -0,0 +1,6 @@
+//Solicitar al usuario que ingrese una palabra y luego 
+//imprimir cada letra en una línea diferente utilizando un bucle for.
+let palabra= prompt("Ingrese una palabra: "); //HOLA COMO ESTÁN 
+for(let letra of palabra){
+    console.log(letra);
+}
